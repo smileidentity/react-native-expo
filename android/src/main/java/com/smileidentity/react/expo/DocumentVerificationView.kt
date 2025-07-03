40c8b707ae6d033c2f41f6656067f910f1b3816c
Harun Wangereka <harunwangereka@gmail.com>
2025-07-04 01:38:31 +0200

    Add Compose views for SmileID Document Verification and SmartSelfie Enrollment

---
+++ android/src/main/java/com/smileidentity/react/expo/DocumentVerificationView.kt
@@ -0,0 +1,26 @@
+package com.smileidentity.react.expo
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.smileidentity.SmileID
+import com.smileidentity.compose.DocumentVerification
+import timber.log.Timber
+
+/**
+ * Compose view that wraps the SmileID document verification screen
+ **/
+@Composable
+fun DocumentVerificationView() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        SmileID.DocumentVerification(
+            countryCode = "KE"
+        ) { result ->
+            Timber.d("DocumentVerification result: $result")
+        }
+    }
+}
---
+++ android/src/main/java/com/smileidentity/react/expo/SmileIDDocumentVerificationView.kt
@@ -0,0 +1,25 @@
+package com.smileidentity.react.expo
+
+import android.content.Context
+import androidx.compose.ui.platform.ComposeView
+import com.smileidentity.compose.DocumentVerification
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+/**
+ * SmartSelfie Enrollment View using ExpoView
+ **/
+class SmileIDDocumentVerificationView(context: Context, appContext: AppContext) :
+    ExpoView(context, appContext) {
+    internal val composeView = ComposeView(context).also {
+
+        it.layoutParams = LayoutParams(
+            LayoutParams.WRAP_CONTENT,
+            LayoutParams.WRAP_CONTENT
+        )
+        it.setContent {
+            DocumentVerificationView()
+        }
+        addView(it)
+    }
+}
