0d8bb27c8b4bc32f6fe1e232a8eb937bb19c4a3f
Harun Wangereka <harunwangereka@gmail.com>
2025-07-04 12:46:57 +0200

    Implement SmileID Document Verification and SmartSelfie Enrollment views in HomeScreen

---
+++ src/SmileIDSmartSelfieEnrollmentModule.ts
@@ -0,0 +1,6 @@
+import {NativeModule} from "expo";
+import {SmileIDExpoModuleEvents} from "./SmileIDExpo.types";
+
+declare class SmileIDSmartSelfieEnrollmentModule extends NativeModule<SmileIDExpoModuleEvents> {
+
+}
