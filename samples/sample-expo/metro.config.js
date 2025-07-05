6e86741f9a44808d8db368477feb606d2d84dbcd
Juma Allan <allanjuma@gmail.com>
2025-07-05 13:34:22 +0300

    fixed build errors

--- samples/sample-expo/metro.config.js
+++ samples/sample-expo/metro.config.js
@@ -19,7 +19,7 @@ config.resolver.nodeModulesPaths = [
 ];
 
 config.resolver.extraNodeModules = {
-  'react-native-expo': '..',
+  'react-native-expo': path.resolve(__dirname, '../../'),
 };
 
 config.watchFolders = [path.resolve(__dirname, '..')];
