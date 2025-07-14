736bd4ecdbee6143d470d7c26b4c8b7a1795045a
Harun Wangereka <harunwangereka@gmail.com>
2025-07-14 13:58:11 +0200

    Fix Android Blank Camera Issue (#23)
    * feat: refactor document verification views to use SmileIDExpoComposeView and add fragment-ktx dependency
    
    * fix: update SmileIDExpoComposeView to manage ViewModelStore and improve lifecycle handling

--- android/src/main/java/com/smileidentity/react/expo/SmileIDExpoModule.kt
+++ android/src/main/java/com/smileidentity/react/expo/SmileIDExpoModule.kt
@@ -1,23 +1,15 @@
 package com.smileidentity.react.expo
 
 import com.smileidentity.SmileID
-import expo.modules.kotlin.Promise
 import expo.modules.kotlin.functions.Coroutine
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import com.smileidentity.models.Config
 
 
 class SmileIDExpoModule : Module() {
-    // Each module class must implement the definition function. The definition consists of components
-    // that describes the module's functionality and behavior.
-    // See https://docs.expo.dev/modules/module-api for more details about available components.
     override fun definition() = ModuleDefinition {
-        // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-        // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-        // The module will be accessible from `requireNativeModule('SmileIDExpo')` in JavaScript.
         Name("SmileIDExpo")
 
         AsyncFunction("initialize") Coroutine { useSandBox: Boolean, enableCrashReporting: Boolean, config: SmileConfigRecord?, apiKey: String? ->
@@ -67,8 +59,6 @@ class SmileIDExpoModule : Module() {
             Prop("config") { view: SmileIDDocumentVerificationView, config: SmileDocumentVerificationRequestRecord ->
                 view.updateConfig(config)
             }
-
-
         }
         View(SmileIDSmartSelfieEnrollmentView::class) {
         }
