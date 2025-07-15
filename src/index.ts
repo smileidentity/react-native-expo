3a2ef2c01736370527e74fb82a8dd078da7e4712
Harun Wangereka <harunwangereka@gmail.com>
2025-07-15 12:33:26 +0300

    feat: add enhanced views for biometric KYC, document verification, and selfie authentication

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,36 @@
 import SmileIDExpoModule from "./SmileIDExpoModule";
 
 export { default } from './SmileIDExpoModule';
+
 import SmileIDSmartSelfieEnrollmentView from './SmileIDSmartSelfieEnrollmentView';
 export { SmileIDSmartSelfieEnrollmentView };
 
+import SmileIDSmartSelfieEnrollmentEnhancedView from './SmileIDSmartSelfieEnrollmentEnhancedView';
+export { SmileIDSmartSelfieEnrollmentEnhancedView };
+
 import SmileIDDocumentVerificationView from './SmileIDDocumentVerificationView';
 export { SmileIDDocumentVerificationView };
+
+import SmileIDDocumentVerificationEnhancedView from './SmileIDDocumentVerificationEnhancedView';
+export { SmileIDDocumentVerificationEnhancedView };
+
+import SmileIDBvnConsentScreenView from './SmileIDBvnConsentScreenView';
+export { SmileIDBvnConsentScreenView };
+
+import SmileIDSmartSelfieAuthenticationView from "./SmileIDSmartSelfieAuthenticationView";
+export { SmileIDSmartSelfieAuthenticationView };
+
+import SmileIDSmartSelfieAuthenticationEnhancedView from "./SmileIDSmartSelfieAuthenticationEnhancedView";
+export { SmileIDSmartSelfieAuthenticationEnhancedView };
+
+import SmileIDBiometricKYCView from "./SmileIDBiometricKYCView";
+export { SmileIDBiometricKYCView };
+
+import SmileIDEnhancedKyCView  from "./SmileIDEnhancedKyCView";
+export { SmileIDEnhancedKyCView };
+
+
+
 
 export * from  './SmileIDExpo.types';
 import type { ExpoConfig } from './SmileIDExpo.types';
