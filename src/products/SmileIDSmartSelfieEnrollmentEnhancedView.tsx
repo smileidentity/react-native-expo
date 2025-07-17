357257c53b2b07839a8e0f64aef0198a19e8d3b5
Harun Wangereka <harunwangereka@gmail.com>
2025-07-17 13:41:46 +0300

    Feat: Implement Enhanced Smart Selfie Enrollment View

--- src/products/SmileIDSmartSelfieEnrollmentEnhancedView.tsx
+++ src/products/SmileIDSmartSelfieEnrollmentEnhancedView.tsx
@@ -1,9 +1,14 @@
 import { requireNativeViewManager } from 'expo-modules-core';
 import * as React from 'react';
 import { ViewProps } from "react-native";
+import {ExpoSmartSelfieEnrollmentRequest} from "../types/SmileIDExpo.types";
 
 
-interface SmileIDSmartSelfieEnrollmentEnhancedViewProps extends ViewProps {}
+interface SmileIDSmartSelfieEnrollmentEnhancedViewProps extends ViewProps {
+    config: ExpoSmartSelfieEnrollmentRequest;
+    onResult?: (result: any) => void;
+    onError?: (error: any) => void;
+}
 
 const NativeView: React.ComponentType<SmileIDSmartSelfieEnrollmentEnhancedViewProps> =
     requireNativeViewManager('SmileIDExpo','SmileIDSmartSelfieEnrollmentEnhancedView' );
