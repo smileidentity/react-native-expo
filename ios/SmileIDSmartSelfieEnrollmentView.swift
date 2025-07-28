beae40b1172f28ec115a433673bb3cb95efb26e2
Harun Wangereka <harunwangereka@gmail.com>
2025-07-28 19:55:48 +0300

    Fix:Force Light mode on all iOS Views (#49)
    * Fix: Force light mode for iOS selfie enrollment view
    
    * Fix: Force light mode for iOS on all views

--- ios/SmileIDSmartSelfieEnrollmentView.swift
+++ ios/SmileIDSmartSelfieEnrollmentView.swift
@@ -18,6 +18,9 @@ final class SmileIDSmartSelfieEnrollmentView: ExpoView {
                 config: nil
             )
         )
+        // Force light mode
+        hostingController.overrideUserInterfaceStyle = .light
+
         super.init(appContext: appContext)
         
         // Set up delegate callbacks
