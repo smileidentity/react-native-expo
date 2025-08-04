6ee9b0081d595b596a5e9c7cb51bc0f3e6d7b0bc
Harun Wangereka <harunwangereka@gmail.com>
2025-08-04 17:09:24 +0300

    Feat: Update wrapper info to use reactNativeExpo for improved clarity

--- ios/SmileIDExpoModule.swift
+++ ios/SmileIDExpoModule.swift
@@ -16,7 +16,7 @@ public class SmileIDExpoModule: Module {
                config: SmileConfig?,
                apiKey: String?
            ) async throws -> Void in
-				 SmileID.setWrapperInfo(name: .reactNative, version: "11.0.0")
+				 SmileID.setWrapperInfo(name: .reactNativeExpo, version: "11.0.0")
            
            if let apiKey = apiKey, let config = config {
                // Case 1: Initialize with API key and config
